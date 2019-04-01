343cdedc599b46b96eb2dc304c65818be13ed848
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-03-31 22:02:41 -0500

    added a timer and a switch to questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,52 @@
 var start = document.querySelector(".startGame");
-var first = document.getElementById("firstQuestion");
+var showQuestion = document.querySelector(".question");
+var choices = document.querySelector("ul");
+var timeWatch = document.getElementById("timer");
 var solution = document.querySelector(".correct");
 var correct = document.querySelector(".correctAnswer");
 
-// var solution = ["Liam Nesson", "4"];
+var triviaAsk = ["Who is the main actor in the Taken Movie?", 
+                 "How many Avengers are there?" ,
+                  "Which of these is not an Avenger?"];
+
+
+
+
+var count = 0;
+
+var watch = 30;
+var intervalID;
+
+var test;
+
 
 start.addEventListener("click", displayQuestion);
 solution.addEventListener("click", updateDisplay);
 
+
+function nextQuestion() {
+    setTimeout(displayQuestion, 30000);
+
+}
+
 function displayQuestion() {   
-    first.style.display = 'block';
+    watch--;
+    timeWatch.innerHTML = watch + " Seconds reamining";
+
+    showQuestion.innerHTML = triviaAsk[count];
+    if (watch === 0) {
+        count++;
+        showQuestion.innerHTML = triviaAsk[count];
+        watch = 30;
+    }
+    showQuestion.style.display = 'block';
+
+    test = setInterval(nextQuestion, 30000);
+
+    choices.style.display = 'block';
     start.style.display = 'none';
+
+    startClock();
 }
 
 function updateDisplay() {
@@ -19,4 +55,10 @@ function updateDisplay() {
 
 }
 
-if (correct === )
+function startClock() {
+    clearInterval(intervalID);
+    intervalID = setInterval(displayQuestion, 1000);
+}
+
+
+
